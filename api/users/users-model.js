d750ca4aa716c5e70187d4e4a03ce96669d20aa2
Canberk Ok <canberk.ok@gmail.com>
2023-07-03 14:35:01 +0300

    users-model ok

--- api/users/users-model.js
+++ api/users/users-model.js
@@ -1,14 +1,17 @@
 /**
   tüm kullanıcıları içeren bir DİZİ ye çözümlenir, tüm kullanıcılar { user_id, username } içerir
  */
-function bul() {
+const db = require('../../data/db-config');
 
+function bul() {
+return db('users').select("user_id","username");
 }
 
 /**
   verilen filtreye sahip tüm kullanıcıları içeren bir DİZİ ye çözümlenir
  */
 function goreBul(filtre) {
+return db('users').where(filtre);
 
 }
 
@@ -16,14 +19,22 @@ function goreBul(filtre) {
   verilen user_id li kullanıcıya çözümlenir, kullanıcı { user_id, username } içerir
  */
 function idyeGoreBul(user_id) {
-
+return db('users').where("user_id",user_id).first();
 }
 
 /**
   yeni eklenen kullanıcıya çözümlenir { user_id, username }
  */
-function ekle(user) {
-
+async function ekle(user) {
+const [id] = await  db('users').insert('user')
+const newUser =await idyeGoreBul(id);  
+return {user_id: newUser.user_id,suername: newUser.username};
 }
 
 // Diğer modüllerde kullanılabilmesi için fonksiyonları "exports" nesnesine eklemeyi unutmayın.
+module.exports = {
+  bul,
+  goreBul,
+  idyeGoreBul,
+  ekle
+}
